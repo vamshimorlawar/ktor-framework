9b25a79e365bd7096d211a8f8f49966a4b2dbe18
vamshimorlawar <vamshimorlawar@gmail.com>
2024-08-27 15:40:31 +0530

    chore: catching exceptions throw in the service layer of application at one place to respond with appropriate status codes and messages

--- src/main/kotlin/example/com/plugins/Routing.kt
+++ src/main/kotlin/example/com/plugins/Routing.kt
@@ -3,6 +3,7 @@ package example.com.plugins
 import example.com.routes.studentRoutes
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -10,6 +11,16 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     install(StatusPages) {
         exception<Throwable> { call, cause ->
+            when(cause) {
+                is NotFoundException -> call.respondText(
+                    text = "404: ${cause.message}",
+                    status = HttpStatusCode.NotFound
+                )
+                is BadRequestException -> call.respondText(
+                    text = "400: ${cause.message}",
+                    status = HttpStatusCode.BadRequest
+                )
+            }
             call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
         }
     }

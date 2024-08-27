962ad9fc14cb88bc168ebd2f6c72ac1fa87f13a6
vamshimorlawar <vamshimorlawar@gmail.com>
2024-08-27 15:44:03 +0530

    feat: implemented student service interface with the crud operations with the validation at places required

--- src/main/kotlin/example/com/services/StudentService.kt
+++ src/main/kotlin/example/com/services/StudentService.kt
@@ -1,7 +1,19 @@
 package example.com.services
 
+import example.com.models.InputStudentType
 import example.com.models.StudentType
 
 interface StudentService {
     suspend fun getAllStudents(): List<StudentType>
+    suspend fun getStudentById(id: Int?): StudentType?
+    suspend fun getStudentByName(name: String): StudentType?
+    suspend fun getStudentBySchool(school: String): StudentType?
+
+    suspend fun addStudent(newStudent: InputStudentType): StudentType?
+
+    suspend fun updateStudent(id: Int?, updatedStudent: InputStudentType): Boolean
+
+    suspend fun deleteStudentById(id: Int?): Boolean
+    suspend fun deleteStudentByName(name: String): Boolean
+    suspend fun deleteStudentBySchool(school: String): Boolean
 }
--- src/main/kotlin/example/com/services/StudentServiceImpl.kt
+++ src/main/kotlin/example/com/services/StudentServiceImpl.kt
@@ -1,19 +1,97 @@
 package example.com.services
 
-import example.com.models.Student
+import example.com.models.InputStudentType
 import example.com.models.StudentType
-import example.com.plugins.dbQuery
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.selectAll
+import example.com.repositories.StudentRepository
+import io.ktor.server.plugins.*
 
-class StudentServiceImpl: StudentService {
-    private fun toStudentType(row: ResultRow): StudentType = StudentType(
-        id = row[Student.id],
-        name = row[Student.name],
-        school = row[Student.school]
-    )
+class StudentServiceImpl(private val repository: StudentRepository) : StudentService {
 
-    override suspend fun getAllStudents(): List<StudentType> = dbQuery {
-        Student.selectAll().map { toStudentType(it) }
+    override suspend fun getAllStudents(): List<StudentType> {
+        return repository.getAllStudents()
+    }
+
+    override suspend fun getStudentById(id: Int?): StudentType? {
+        if (id == null) {
+            throw BadRequestException("Invalid ID for a student received")
+        }
+        val student = repository.getStudentById(id)
+        if (student == null) {
+            throw NotFoundException("Student with ID $id doesn't exist")
+        } else {
+            return student
+        }
+    }
+
+    override suspend fun getStudentByName(name: String): StudentType? {
+        val student = repository.getStudentByName(name)
+        if (student == null) {
+            throw NotFoundException("Student with Name $name doesn't exist")
+        } else {
+            return student
+        }
+    }
+
+    override suspend fun getStudentBySchool(school: String): StudentType? {
+        val student = repository.getStudentBySchool(school)
+        if (student == null) {
+            throw NotFoundException("Student with School $school doesn't exist")
+        } else {
+            return student
+        }
+    }
+
+    override suspend fun addStudent(newStudent: InputStudentType): StudentType? {
+        val student = repository.addStudent(newStudent)
+        if(student == null){
+//            TODO(What exception should be thrown here? Internal Server Error)
+            throw Exception("ADD New Student failed, returned null from the repository")
+        }else{
+            return student
+        }
+    }
+
+    override suspend fun updateStudent(id: Int?, updatedStudent: InputStudentType): Boolean {
+        if(id == null){
+            throw BadRequestException("Invalid ID for a student received")
+        }
+        val response = repository.updateStudent(id, updatedStudent)
+//        TODO(How to know UPDATE failed because of the ID not found or some other reason?)
+        if(!response){
+            throw Exception("UPDATE Student failed, returned false from the repository")
+        }else{
+            return true
+        }
+    }
+
+    override suspend fun deleteStudentById(id: Int?): Boolean {
+        if(id == null){
+            throw BadRequestException("Invalid ID for a student received")
+        }
+        val response = repository.deleteStudentById(id)
+//        TODO(How to know UPDATE failed because of the ID not found or some other reason?)
+        if(!response){
+            throw Exception("DELETE Student failed, returned false from the repository")
+        }else{
+            return true
+        }
+    }
+
+    override suspend fun deleteStudentByName(name: String): Boolean {
+        val response = repository.deleteStudentByName(name)
+        if(!response){
+            throw Exception("DELETE Student failed, returned false from the repository")
+        }else{
+            return true
+        }
+    }
+
+    override suspend fun deleteStudentBySchool(school: String): Boolean {
+        val response = repository.deleteStudentBySchool(school)
+        if(!response){
+            throw Exception("DELETE Student failed, returned false from the repository")
+        }else{
+            return true
+        }
     }
 }

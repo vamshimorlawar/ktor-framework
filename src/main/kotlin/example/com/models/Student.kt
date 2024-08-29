793b159d8ddef24a76710d6289e4378372468383
vamshimorlawar <vamshimorlawar@gmail.com>
2024-08-29 12:17:18 +0530

    fix: used nullable ?=null in data class instead of creating whole new data class for req, res

--- src/main/kotlin/example/com/models/Student.kt
+++ src/main/kotlin/example/com/models/Student.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
-object Student: Table() {
+object Student : Table() {
     val id: Column<Int> = integer("id").autoIncrement()
     val name = varchar("name", 255)
     val school = varchar("school", 255)
@@ -12,16 +12,10 @@ object Student: Table() {
     override val primaryKey = PrimaryKey(id)
 }
 
-//  TODO(Is it good practice to create multiple data classes to define the different return data types?)
+//  TODO(Is it good practice to create multiple data classes to define the different return data types?) -> JSON Ignore
 @Serializable
 data class StudentType(
-    val id: Int,
-    val name: String,
-    val school: String
-)
-
-@Serializable
-data class InputStudentType(
+    val id: Int? = null,
     val name: String,
     val school: String
 )
